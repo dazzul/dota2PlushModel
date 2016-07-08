0eef06daf3d5492c2f796512a22056f7f38ffd8e
dazzul <uchihajax@gmail.com>
2016-07-08 16:07:26 +0100

    Add files via upload
    initial commit

---
+++ Plushies/Plushies/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plushies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Go();
+            }
+
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        static void Go()
+        {
+            var r = new Random();
+            var diffPlush = 15;
+            int sales = 10000;
+            int itemsPerTx = 10;
+            int totalStock = sales * itemsPerTx;
+            int stockOfEach = totalStock / diffPlush; // fuck rounding because fraction of plushie is not plushie
+            int stockOfEachReally = stockOfEach * diffPlush; // we may have lost some to rounding
+            var stock = new List<int>();
+
+            for (int i = 0; i < diffPlush; i++)
+            {
+                for (int j = 0; j < stockOfEach; j++)
+                {
+                    stock.Add(i); // doesn't matter if we stock them deterministically as we extract them randomly
+                }
+            }
+
+            var dupes = new List<int>();
+            for (int i = 0; i < itemsPerTx; i++) dupes.Add(0); // initialise our duplicates map
+            // sell em till they're all sold
+            while (stock.Count > 0)
+            {
+                // someone is going to buy some
+                int numberToBuy = stock.Count < itemsPerTx ? stock.Count : itemsPerTx; // either take items per tx or stock count, whichever lower
+                var shoppingCart = new List<int>();
+                // we buy many
+                for (int j = 0; j < numberToBuy; j++)
+                {
+                    // we buy one with randomisation.
+                    int randomItem = r.Next(0, stock.Count);
+                    shoppingCart.Add(stock[randomItem]);
+                    stock.RemoveAt(randomItem);
+                }
+
+                // unboxing time!
+                var hash = new Dictionary<int, int>();
+                // whats our worst duplicates case?
+                foreach (var item in shoppingCart)
+                {
+                    if (hash.ContainsKey(item)) hash[item]++;
+                    else hash.Add(item, 0);
+                }
+                var winner = hash.Max(x => x.Value);
+                dupes[winner]++;
+            }
+
+            // results
+            for (int i = 0; i < dupes.Count; i++)
+            {
+                Console.WriteLine("{0} dupes happened: {1} times", i, dupes[i]);
+            }
+
+            var dir = @"c:\users\username\documents\plushies\data";
+            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+            var filePath = Path.Combine(dir, "Plushie_Sim_" + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss") + ".csv");
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("{0},{1},{2}", "# dupes", String.Empty, "# times");
+                    for (int i = 0; i < dupes.Count; i++)
+                    {
+                        writer.WriteLine("{0},{1},{2},{3}", i, "dupes happened: ", dupes[i], "times");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Plushies/PlushiesAverage/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlushiesAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // give me files made by the other program
+            var dir = @"c:\users\username\documents\plushies\data";
+            var outDir = @"c:\users\username\documents\plushies\avg";
+
+            var allDupes = Process(dir);
+      
+            var avgs = new Dictionary<int, double>();
+
+            // we know there are 10 rows per file
+            for (int i = 0; i < 10; i++)
+            {
+                var toAverage = new List<int>();
+                foreach(var hash in allDupes)
+                {
+                    // but do we?
+                    if (hash.Count != 10) throw new InvalidOperationException("Incorrect file");
+                    toAverage.Add(hash[i]);
+                }
+                avgs.Add(i, toAverage.Average()); 
+            }
+            foreach(var kp in avgs)
+            {
+                Console.WriteLine("{0} - average: {1}", kp.Key, kp.Value);
+            }
+            if (!Directory.Exists(outDir)) Directory.CreateDirectory(outDir);
+            Write(Path.Combine(outDir, "Plushie_Avg_" + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss") + ".csv"), avgs);
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        private static void Write(string filePath, Dictionary<int, double> avgs)
+        {
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("# of dupes", "average");
+                    foreach (var kp in avgs)
+                    {
+                        writer.WriteLine("{0},{1}", kp.Key, kp.Value);
+                    }
+                }
+            }
+        }
+
+        private static List<Dictionary<int, int>> Process(string directory)
+        {
+            var allDupes = new List<Dictionary<int, int>>();
+            var info = new DirectoryInfo(directory);
+            if (!info.Exists) throw new ArgumentException("directory: " + directory + " does not exist");
+            var files = info.GetFiles();
+            foreach (var file in files)
+            {
+                allDupes.Add(ProcessFile(file.FullName));
+            }
+            foreach (var dir in info.GetDirectories())
+            {
+                allDupes.AddRange(Process(dir.FullName));
+            }
+
+            return allDupes;
+        }
+
+        private static Dictionary<int, int> ProcessFile(string filePath)
+        {
+            var obj = new Dictionary<int, int>();
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                using (TextReader reader = new StreamReader(stream))
+                {
+                    // no headers pls
+                    reader.ReadLine();
+                    string all = reader.ReadToEnd();
+                    string[] data = all.Split(Environment.NewLine.ToCharArray());
+                    foreach (string line in data)
+                    {
+                        if (String.IsNullOrEmpty(line)) continue; // shit hack, my split is wrong, this "fixes" it
+                        string[] lineData = line.Split(','); // naive but okay cause I know there are no , symbols in here aside from cell spacers
+                        if (lineData.Length != 4)
+                        {
+                            throw new InvalidOperationException("Invalid line");
+                        }
+                        obj.Add(Convert.ToInt32(lineData[0]), Convert.ToInt32(lineData[2]));
+                    }
+                }
+                return obj;
+            }
+        }
+    }
+}
